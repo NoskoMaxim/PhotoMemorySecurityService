266e785828e63d2005ad581330dab5c6b2afd459
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-03-22 23:37:43 +0300

    ADDED Security Configuration

--- src/main/java/com/photomemorysecurityservice/PhotoMemorySecurityApplication.java
+++ src/main/java/com/photomemorysecurityservice/PhotoMemorySecurityApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PhotoMemorySecurityApplication {
@@ -17,5 +19,10 @@ public class PhotoMemorySecurityApplication {
         return args -> System.out.println(
                 "Photo Memory Security Application is active and ready to go!\n" +
                         "Swagger Documentation: http://localhost:8082/swagger-ui/#/");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
