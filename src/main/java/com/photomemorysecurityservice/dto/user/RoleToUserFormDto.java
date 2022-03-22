55f1f81b3c1584dba50e82520f113ab137dfe2ab
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-03-22 20:41:37 +0300

    ADDED same Dtos for User

---
+++ src/main/java/com/photomemorysecurityservice/dto/user/RoleToUserFormDto.java
@@ -0,0 +1,26 @@
+package com.photomemorysecurityservice.dto.user;
+
+public class RoleToUserFormDto {
+
+    private String username;
+    private String roleName;
+
+    public RoleToUserFormDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
---
+++ src/main/java/com/photomemorysecurityservice/dto/user/UsernameAndPasswordToCreateFormDto.java
@@ -0,0 +1,18 @@
+package com.photomemorysecurityservice.dto.user;
+
+public class UsernameAndPasswordToCreateFormDto {
+
+    private String username;
+    private String password;
+
+    public UsernameAndPasswordToCreateFormDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
