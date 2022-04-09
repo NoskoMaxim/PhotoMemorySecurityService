8311a301a917068ee29499140b442352815b1969
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-04-09 23:48:16 +0300

    ADDED User Rest Controller

---
+++ src/main/java/com/photomemorysecurityservice/controller/user/UserController.java
@@ -0,0 +1,92 @@
+package com.photomemorysecurityservice.controller.user;
+
+import com.photomemorysecurityservice.dto.OperationMessageDto;
+import com.photomemorysecurityservice.dto.user.RoleToUserFormDto;
+import com.photomemorysecurityservice.dto.user.UserRoleDto;
+import com.photomemorysecurityservice.dto.user.UserToUpdateFormDto;
+import com.photomemorysecurityservice.dto.user.UsernameAndPasswordToCreateFormDto;
+import com.photomemorysecurityservice.service.user.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/photomemory")
+public class UserController {
+
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/user/create",
+            consumes = {APPLICATION_JSON_VALUE})
+    public ResponseEntity createUser(@RequestBody UsernameAndPasswordToCreateFormDto createForm) {
+        userService.createUser(createForm);
+        return ResponseEntity.ok(new OperationMessageDto("Successful operation"));
+    }
+
+    @PutMapping(value = "/user/update",
+            consumes = {APPLICATION_JSON_VALUE})
+    public ResponseEntity updateUser(@RequestBody UserToUpdateFormDto userDto) {
+        userService.updateUser(userDto);
+        return ResponseEntity.ok(new OperationMessageDto("Successful operation"));
+    }
+
+    @GetMapping(value = "/user/get/all",
+            produces = {APPLICATION_JSON_VALUE})
+    public ResponseEntity getAllUsers() {
+        return ResponseEntity.ok().body(userService.getAllUsers());
+    }
+
+    @GetMapping(value = "/user/get/{username}",
+            produces = {APPLICATION_JSON_VALUE})
+    public ResponseEntity getUserByUsername(@PathVariable String username) {
+        return ResponseEntity.ok().body(userService.getUserByUsername(username));
+    }
+
+    @PostMapping(value = "/role/create")
+    public ResponseEntity createRole(@RequestParam String roleName) {
+        userService.createRole(roleName);
+        return ResponseEntity.ok(new OperationMessageDto("Successful operation"));
+    }
+
+    @PostMapping(value = "/role/add",
+            consumes = {APPLICATION_JSON_VALUE})
+    public ResponseEntity addRoleToUser(@RequestBody RoleToUserFormDto roleForm) {
+        userService.addRoleToUser(roleForm);
+        return ResponseEntity.ok(new OperationMessageDto("Successful operation"));
+    }
+
+    @PutMapping(value = "/role/update",
+            consumes = {APPLICATION_JSON_VALUE})
+    public ResponseEntity updateRole(@RequestBody UserRoleDto roleDto) {
+        userService.updateRole(roleDto);
+        return ResponseEntity.ok(new OperationMessageDto("Successful operation"));
+    }
+
+    @DeleteMapping(value = "/role/delete/{roleId}")
+    public ResponseEntity deleteRole(@PathVariable Long roleId) {
+        userService.deleteRole(roleId);
+        return ResponseEntity.ok(new OperationMessageDto("Successful operation"));
+    }
+
+    @GetMapping(value = "/role/get/all",
+            produces = {APPLICATION_JSON_VALUE})
+    public ResponseEntity getAllRoles() {
+        return ResponseEntity.ok().body(userService.getAllRoles());
+    }
+
+    @GetMapping(value = "/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        userService.refreshToken(request, response);
+    }
+}
