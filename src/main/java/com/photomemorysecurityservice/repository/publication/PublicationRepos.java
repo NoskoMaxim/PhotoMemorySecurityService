48ad90777a1015f2b2a45d74174db64aeff8448b
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-04-15 21:03:57 +0300

    REFACTORED Publication Repository Method

--- src/main/java/com/photomemorysecurityservice/repository/publication/PublicationRepos.java
+++ src/main/java/com/photomemorysecurityservice/repository/publication/PublicationRepos.java
@@ -4,7 +4,9 @@ import com.photomemorysecurityservice.model.publication.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PublicationRepos extends JpaRepository<Publication, Long> {
-
+    Optional<Publication> findPublicationByFileName(String fileName);
 }
