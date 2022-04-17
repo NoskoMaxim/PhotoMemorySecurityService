1d0df31d23bfa7bebe364f117ff8ecb2fba85aa7
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-04-17 14:57:01 +0300

    REFACTORED Publication Controller

--- src/main/java/com/photomemorysecurityservice/controller/fileloader/PublicationController.java
+++ src/main/java/com/photomemorysecurityservice/controller/fileloader/PublicationController.java
@@ -2,7 +2,7 @@ package com.photomemorysecurityservice.controller.fileloader;
 
 import com.photomemorysecurityservice.dto.OperationMessageDto;
 import com.photomemorysecurityservice.dto.publication.PublicationDto;
-import com.photomemorysecurityservice.dto.publication.TextForCreatePublicationDto;
+import com.photomemorysecurityservice.service.fileloader.PublicationService;
 import com.photomemorysecurityservice.service.fileloader.PublicationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,37 +10,33 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.HashMap;
 
-import static org.springframework.http.MediaType.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
-@RequestMapping("/photomemory/file")
+@RequestMapping("/photomemory/publication")
 public class PublicationController {
 
     private final PublicationServiceImpl publicationService;
 
     @Autowired
-    public PublicationController(PublicationServiceImpl fileLoaderService) {
-        this.publicationService = fileLoaderService;
+    public PublicationController(PublicationServiceImpl publicationService) {
+        this.publicationService = publicationService;
     }
 
     @PostMapping(
-            path = "/{userId}",
-            consumes = {
-                    MULTIPART_FORM_DATA_VALUE,
-                    APPLICATION_JSON_VALUE
-            },
+            path = "/file/{userId}",
+            consumes = MULTIPART_FORM_DATA_VALUE,
             produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<OperationMessageDto> uploadPhotoForPublication(
             @PathVariable(value = "userId") Long userId,
-            @RequestPart("file") MultipartFile file,
-            @RequestBody TextForCreatePublicationDto textDto) throws IOException {
-        publicationService.createPublication(userId, file, textDto.getText());
+            @RequestPart("file") MultipartFile file) throws IOException {
+        publicationService.createPublication(userId, file);
         return ResponseEntity.ok(new OperationMessageDto("Successful operation"));
     }
 
-    @GetMapping(path = "/{photoId}",
+    @GetMapping(path = "/get/{photoId}",
             produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<PublicationDto> getPublication(
             @PathVariable Long photoId) {
