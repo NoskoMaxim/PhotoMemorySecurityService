a52a44104438f8269e3cf101debfd90f1a146c70
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-04-20 22:57:52 +0300

    ADDED Same Endpoints for Publication in Controller

--- src/main/java/com/photomemorysecurityservice/controller/fileloader/PublicationController.java
+++ src/main/java/com/photomemorysecurityservice/controller/fileloader/PublicationController.java
@@ -2,7 +2,7 @@ package com.photomemorysecurityservice.controller.fileloader;
 
 import com.photomemorysecurityservice.dto.OperationMessageDto;
 import com.photomemorysecurityservice.dto.publication.PublicationDto;
-import com.photomemorysecurityservice.service.fileloader.PublicationService;
+import com.photomemorysecurityservice.dto.publication.TextForCreatePublicationDto;
 import com.photomemorysecurityservice.service.fileloader.PublicationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
@@ -26,7 +27,7 @@ public class PublicationController {
     }
 
     @PostMapping(
-            path = "/file/{userId}",
+            path = "/{userId}",
             consumes = MULTIPART_FORM_DATA_VALUE,
             produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<OperationMessageDto> uploadPhotoForPublication(
@@ -36,10 +37,31 @@ public class PublicationController {
         return ResponseEntity.ok(new OperationMessageDto("Successful operation"));
     }
 
-    @GetMapping(path = "/get/{photoId}",
+    @GetMapping(path = "/get/photo/{photoId}",
             produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<PublicationDto> getPublication(
             @PathVariable Long photoId) {
         return ResponseEntity.ok().body(publicationService.getPublication(photoId));
+    }
+
+    @GetMapping(path = "/get/all",
+            produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<PublicationDto>> getAllPublications() {
+        return ResponseEntity.ok().body(publicationService.getAllPublications());
+    }
+
+    @PostMapping(path = "/set/text",
+            consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<OperationMessageDto> setTextForPublication(
+            @RequestBody TextForCreatePublicationDto textDto) {
+        publicationService.setTextForPublication(textDto);
+        return ResponseEntity.ok(new OperationMessageDto("Successful operation"));
+    }
+
+    @GetMapping(path = "/get/{userId}",
+            produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<PublicationDto>> getAllPublicationByUserWithUserId(
+            @PathVariable Long userId) {
+        return ResponseEntity.ok().body(publicationService.getAllPublicationByUserWithUserId(userId));
     }
 }
