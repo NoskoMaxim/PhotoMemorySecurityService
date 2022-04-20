828f5e449355ad4ee69ccaffbe74b9dff04f82d8
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-04-20 22:52:38 +0300

    ADDED Dto class for Publication

--- src/main/java/com/photomemorysecurityservice/dto/publication/TextForCreatePublicationDto.java
+++ src/main/java/com/photomemorysecurityservice/dto/publication/TextForCreatePublicationDto.java
@@ -1,13 +1,23 @@
 package com.photomemorysecurityservice.dto.publication;
 
 public class TextForCreatePublicationDto {
+    private Long publicationId;
     private String text;
 
-    public TextForCreatePublicationDto(String text) {
+    public TextForCreatePublicationDto(Long publicationId, String text) {
+        this.publicationId = publicationId;
         this.text = text;
     }
 
     public TextForCreatePublicationDto() {
+    }
+
+    public Long getPublicationId() {
+        return publicationId;
+    }
+
+    public void setPublicationId(Long publicationId) {
+        this.publicationId = publicationId;
     }
 
     public String getText() {
