72f84d32770640e1d7ea506eeb85110d58757741
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-04-20 22:54:23 +0300

    ADDED Query Method for Publication in Repository

--- src/main/java/com/photomemorysecurityservice/repository/publication/PublicationRepos.java
+++ src/main/java/com/photomemorysecurityservice/repository/publication/PublicationRepos.java
@@ -4,9 +4,12 @@ import com.photomemorysecurityservice.model.publication.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PublicationRepos extends JpaRepository<Publication, Long> {
     Optional<Publication> findPublicationByFileName(String fileName);
+
+    List<Publication> findPublicationsByUser_UserId(Long userId);
 }
