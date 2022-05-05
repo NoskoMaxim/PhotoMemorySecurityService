bfb48f46362cb8ffd35cb74e82526ad8603eaffe
Nosko Maxim <noskomaximbsu2020@gmail.com>
2022-05-06 00:21:44 +0300

    ADDED Mapping for Comment

--- src/main/java/com/photomemorysecurityservice/adapter/publication/PublicationAdapter.java
+++ src/main/java/com/photomemorysecurityservice/adapter/publication/PublicationAdapter.java
@@ -1,6 +1,8 @@
 package com.photomemorysecurityservice.adapter.publication;
 
+import com.photomemorysecurityservice.dto.publication.CommentDto;
 import com.photomemorysecurityservice.dto.publication.PublicationDto;
+import com.photomemorysecurityservice.model.publication.Comments;
 import com.photomemorysecurityservice.model.publication.Publication;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
@@ -9,16 +11,25 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.modelmapper.convention.MatchingStrategies.LOOSE;
+
 @Component
 public class PublicationAdapter {
     ModelMapper publicationMapper = new ModelMapper();
 
     public PublicationAdapter() {
+        this.publicationMapper.getConfiguration().setMatchingStrategy(LOOSE);
         TypeMap<Publication, PublicationDto> propertyMapperPublicationToPublicationDto
                 = this.publicationMapper.createTypeMap(Publication.class, PublicationDto.class);
         propertyMapperPublicationToPublicationDto.addMappings(publicationMapper ->
                 publicationMapper.map(publication ->
                         publication.getUser().getUsername(), PublicationDto::setUsername));
+        TypeMap<Comments, CommentDto> propertyMapperCommentsToCommentsDto
+                = this.publicationMapper.createTypeMap(Comments.class, CommentDto.class);
+        propertyMapperCommentsToCommentsDto.addMappings(publicationMapper -> {
+            publicationMapper.map(comments ->
+                    comments.getUser().getUsername(), CommentDto::setUsername);
+        });
     }
 
     public PublicationDto getPublicationDto(Publication publication) {
